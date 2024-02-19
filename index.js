8ce7f2686d82070664975f325de51649cf2c9b7b
German Burgardt <german@getgloby.com>
2024-02-18 22:06:49 -0300

    Improved directory handling in index.js and refactored download function for better error handling.\\\\

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
 #!/usr/bin/env node
 import ytdl from "ytdl-core";
-import fs from "fs";
 import path from "path";
 import Moises from "moises/sdk.js";
 import dotenv from "dotenv";
+import os from "os";
 
 dotenv.config();
+import fs from "fs";
+
+import * as fsPromises from "fs";
 
 const moises = new Moises({ apiKey: process.env.ROCKAI_MOISES_API_KEY });
+
+async function ensureDirectoryExistence(dirPath) {
+  try {
+    await fsPromises.promises.mkdir(dirPath, { recursive: true }); // Usa await con fs.promises.mkdir
+    console.log(`Directory created or already exists: ${dirPath}`);
+  } catch (error) {
+    console.error(`Error creating directory: ${dirPath}`, error);
+  }
+}
+
+async function processAudio(youtubeURL) {
+  console.log(`Downloading audio from: ${youtubeURL}`);
+
+  const workflow = "test1";
+
+  const audioUrl = path.join(os.homedir(), "rockai/audio");
+  const stemUrl = path.join(os.homedir(), "rockai/stems");
+
+  try {
+    await ensureDirectoryExistence(audioUrl);
+    await ensureDirectoryExistence(stemUrl);
+
+    const audioPath = path.join(audioUrl, "downloadedAudio.wav");
+    await downloadYouTubeAudio(youtubeURL, audioPath);
+    console.log("Audio downloaded");
+    console.log("audioUrl", audioUrl);
+    await moises.processFolder(workflow, audioUrl, stemUrl, {});
+    console.log(`Audio processed`);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
 
 async function downloadYouTubeAudio(url, outputPath) {
   return new Promise((resolve, reject) => {
@@ -18,25 +53,6 @@ async function downloadYouTubeAudio(url, outputPath) {
   });
 }
 
-async function processAudio(youtubeURL) {
-  console.log(`Downloading audio from: ${youtubeURL}`);
-
-  const workflow = "test1";
-
-  const audioUrl = "~/rockai/audio";
-  const stemUrl = "~/rockai/stems";
-
-  try {
-    const audioPath = path.join(audioUrl, "downloadedAudio.wav");
-    await downloadYouTubeAudio(youtubeURL, audioPath);
-    console.log("Audio downloaded");
-    await moises.processFolder(workflow, audioUrl, stemUrl, {});
-    console.log(`Audio processed`);
-  } catch (error) {
-    console.error("Error:", error);
-  }
-}
-
 const [youtubeURL] = process.argv.slice(2);
 
 if (!youtubeURL) {
@@ -44,4 +60,6 @@ if (!youtubeURL) {
   process.exit(1);
 }
 
-processAudio(youtubeURL);
+processAudio(youtubeURL).then(() => {
+  console.log("Done");
+});
